73dbd1afdcf1725a00d700e35d82428c4f4e5854
Joose Lohi <joose.lohi01@gmail.com>
2022-09-08 14:12:42 +0300

    add FMIUrl.java

---
+++ ApiFMI/src/main/java/FMIUrl.java
@@ -0,0 +1,11 @@
+/**
+ * This class is used to generate the urls to fetch data
+ * from the Finnish Meteorological Institute (FMI).
+ * @author J.L.
+ */
+public class FMIUrl {
+
+    static String DETAILED_INSTRUCTIONS = "http://opendata.fmi.fi/wfs?service=WFS&"+
+                                          "version=2.0.0&request=describeStoredQueries";
+
+}
