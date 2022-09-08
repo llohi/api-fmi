5254388ae912eec002d2e98eb9fbaccf75d5d503
Joose Lohi <joose.lohi01@gmail.com>
2022-09-08 14:33:38 +0300

    test classes

--- ApiFMI/src/main/java/Main.java
+++ ApiFMI/src/main/java/Main.java
@@ -1,9 +1,19 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 /**
  * This class is used to test the functionality of other classes.
  * @author J.L.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+            throws IOException, ParserConfigurationException, SAXException {
+
+
+        ServerRequest.getObject(
+                FMIUrl.DETAILED_INSTRUCTIONS);
+
     }
 }
