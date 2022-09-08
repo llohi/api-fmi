5a2d2ca9f051f722ebfb3341680c649873854293
Joose Lohi <joose.lohi01@gmail.com>
2022-09-08 14:02:32 +0300

    add Main.java

---
+++ ApiFMI/src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * This class is used to test the functionality of other classes.
+ * @author J.L.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
