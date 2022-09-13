354c1ad4c764b92ac56093497b4f756d43dcb2de
Joose Lohi <joose.lohi01@gmail.com>
2022-09-13 10:04:55 +0300

    add methods getForecastURL() and getStatURL() to FMIUrl.java

--- ApiFMI/src/main/java/FMIUrl.java
+++ ApiFMI/src/main/java/FMIUrl.java
@@ -21,32 +21,33 @@ public class FMIUrl {
 
     /**
      * This method returns an url formatted according to the parameters
-     * to fetch data from the FMI api.
+     * to fetch observed data from the FMI api.
      * @param temp does user want temperature data
-     * @param obs_wind does user want observed wind data
-     * @param obs_cloud does user want observed cloudiness data
-     * @param pred_wind does user want predicted wind data
-     * @param pred_temp does user want predicted temperature data
+     * @param wind does user want observed wind data
+     * @param cloudiness does user want observed cloudiness data
      * @param x_min minimum x coordinate
      * @param y_min minimum y coordinate
      * @param x_max maximum x coordinate
      * @param y_max maximum y coordinate
+     * @param start_time the starting time
+     * @param end_time the ending time
      * @param timestep the timestep that seperates the data
      * @return the formatted url as a String
      */
-    static String getURL(boolean temp, boolean obs_wind, boolean obs_cloud,
-                         boolean pred_wind, boolean pred_temp,
-                         int x_min, int y_min, int x_max, int y_max,
-                         int timestep) {
-        // TODO: 13.9.2022 Add start_time and end_time parameters
+    static String getObservedURL(boolean temp, boolean wind, boolean cloudiness,
+                         double x_min, double y_min, double x_max, double y_max,
+                         String start_time, String end_time, int timestep) {
 
         String url = ROOT;
 
-        // Configure data format
+        // Configure query
         url += "&storedquery_id=fmi::observations::weather::simple";
 
         // Add coordinates
         url += "&bbox="+x_min+","+y_min+","+x_max+","+y_max;
+
+        // Add time interval
+        url += "&starttime="+start_time+"&endtime="+end_time;
 
         // Add timestep
         url += "&timestep="+timestep;
@@ -57,14 +58,67 @@ public class FMIUrl {
         if (temp)
             url += "t2m,";
 
-        if (obs_wind)
+        if (wind)
             url += "ws_10min,";
 
-        if (obs_cloud)
+        if (cloudiness)
             url += "n_man,";
 
         return url.substring(0, url.length() - 1);  // Remove extra comma
     }
 
+    static String getForecastURL(double lat, double lon,
+                                 String start_time, String end_time,
+                                 int timestep,
+                                 boolean temp, boolean wind) {
+
+        String url = ROOT;
+
+        // Configure query
+        url += "&storedquery_id=fmi::forecast::harmonie::surface::point::simple";
+
+        // Add latitude and longitude
+        url += "&latlon="+lat+","+lon;
+
+        url += "&starttime="+start_time+"&endtime="+end_time;
+        url += "&timestep="+timestep;
+
+        // Add parameters
+        url += "&parameters=";
+
+        if (temp)
+            url += "temperature,";
+
+        if (wind)
+            url += "windspeedms,";
+
+        return url.substring(0, url.length() - 1);  // Remove extra comma
+    }
+
+    static String getStatURL(double x_min, double y_min, double x_max, double y_max,
+                             String start_time, String end_time,
+                             boolean avg, boolean min, boolean max) {
+
+        String url = ROOT;
+
+        // Configure query
+        url += "&storedquery_id=fmi::observations::weather::hourly::simple";
+        url += "&bbox="+x_min+","+y_min+","+x_max+","+y_max;
+        url += "&starttime="+start_time+"&endtime="+end_time;
+
+        // Add parameters
+        url += "&parameters=";
+        if (avg)
+            url += "TA_PT1H_AVG,";
+
+        if (min)
+            url += "TA_PT1H_MIN,";
+
+        if (max)
+            url += "TA_PT1H_MAX,";
+
+
+        return url.substring(0, url.length() - 1);
+    }
 
 }
